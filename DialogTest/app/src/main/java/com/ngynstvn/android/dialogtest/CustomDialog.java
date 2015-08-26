5ca900c147e8bee5de367505dfc8e59f514402eb
Steven Nguyen <ngynstvn@gmail.com>
2015-08-25 20:14:20 -0700

    Added positive and negative buttons instead. Also had to change the ripple effect xml in order to make sure the button's background is the window's background.

--- DialogTest/app/src/main/java/com/ngynstvn/android/dialogtest/CustomDialog.java
+++ DialogTest/app/src/main/java/com/ngynstvn/android/dialogtest/CustomDialog.java
@@ -3,9 +3,17 @@ package com.ngynstvn.android.dialogtest;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class CustomDialog extends DialogFragment {
+
+    private static final String TAG = "Test (" + CustomDialog.class.getSimpleName() + "): ";
+
+    private AlertDialog.Builder builder;
+    private AlertDialog alertDialog;
 
     // Important single instantiation of this class
 
@@ -26,9 +34,24 @@ public class CustomDialog extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new AlertDialog.Builder(getActivity())
+
+        return new AlertDialog.Builder(getActivity(), R.style.MaterialAlertDialogStyle)
                 .setView(getActivity().getLayoutInflater().inflate(R.layout.dialog_layout, null))
-                .create();
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.v(TAG, "Positive Button Clicked");
+                        Toast.makeText(getActivity(), "Positive Button Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.v(TAG, "Negative Button Clicked");
+                        Toast.makeText(getActivity(), "Negative Button Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                }).create();
+
     }
 
 }
