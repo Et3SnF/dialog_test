7af8413f0008a922fbfefd10ee265762d657717b
Steven Nguyen <ngynstvn@gmail.com>
2015-08-25 22:33:57 -0700

    Added functionality and added lifecycle methods, studied the order of executions for them. Also learned about inflating views in dialog fragments are different than usual fragments.

--- DialogTest/app/src/main/java/com/ngynstvn/android/dialogtest/CustomDialog.java
+++ DialogTest/app/src/main/java/com/ngynstvn/android/dialogtest/CustomDialog.java
@@ -1,19 +1,21 @@
 package com.ngynstvn.android.dialogtest;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class CustomDialog extends DialogFragment {
 
     private static final String TAG = "Test (" + CustomDialog.class.getSimpleName() + "): ";
 
-    private AlertDialog.Builder builder;
-    private AlertDialog alertDialog;
+    private Button btnAddCategory;
 
     // Important single instantiation of this class
 
@@ -32,11 +34,35 @@ public class CustomDialog extends DialogFragment {
 
     }
 
+    // ----- Lifecycle Methods ----- //
+
+    @Override
+    public void onAttach(Activity activity) {
+        Log.v(TAG, "onAttach() called");
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.v(TAG, "onCreate() called");
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        return new AlertDialog.Builder(getActivity(), R.style.MaterialAlertDialogStyle)
-                .setView(getActivity().getLayoutInflater().inflate(R.layout.dialog_layout, null))
+        Log.v(TAG, "onCreateDialog() called");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(),
+                R.style.MaterialAlertDialogStyle);
+
+        View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_layout, null);
+
+        // Inflate anything specifically in this lifecycle method
+
+        btnAddCategory = (Button) view.findViewById(R.id.btn_add_category);
+
+        builder.setView(view)
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -50,8 +76,76 @@ public class CustomDialog extends DialogFragment {
                         Log.v(TAG, "Negative Button Clicked");
                         Toast.makeText(getActivity(), "Negative Button Clicked", Toast.LENGTH_SHORT).show();
                     }
-                }).create();
+                });
+
+        return builder.create();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+
+        Log.v(TAG, "onActivityCreated() called");
+
+        super.onActivityCreated(savedInstanceState);
+
+        btnAddCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "Add Category Button Clicked");
+                Toast.makeText(getActivity(), "Add Category Button Clicked", Toast.LENGTH_SHORT).show();
+                dismiss(); // To dismiss the dialog..really..use this if you need to.
+            }
+        });
 
     }
+
+    @Override
+    public void onStart() {
+        Log.v(TAG, "onStart() called");
+        super.onStart();
+        // This is where the fragment stops at when I first load it.
+    }
+
+    @Override
+    public void onCancel(DialogInterface dialog) {
+        Log.v(TAG, "onCancel() called");
+        // This is called only if I touched outside of the dialog to dismiss
+        super.onCancel(dialog);
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        Log.v(TAG, "onDismiss() called");
+        super.onDismiss(dialog);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        Log.v(TAG, "onSaveInstanceState() called");
+        super.onSaveInstanceState(outState);
+
+        // Called probably whenever I have another dialog and that I want to call to do something
+        // to update information here
+    }
+
+    @Override
+    public void onStop() {
+        Log.v(TAG, "onStop() called");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.v(TAG, "onDestroyView() called");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.v(TAG, "onDetach() called");
+        super.onDetach();
+    }
+
+    // ---------------- //
 
 }
